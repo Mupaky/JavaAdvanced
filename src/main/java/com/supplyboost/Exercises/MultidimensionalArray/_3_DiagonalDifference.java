5d9162b707f29e4ab6054599708e740506bf7a14
Mupaky <georgi.jelev94@gmail.com>
2024-09-13 20:57:12 +0300

    First 4 tasks from Multidimensional Arrays Exercises

--- src/main/java/com/supplyboost/Exercises/MultidimensionalArray/_3_DiagonalDifference.java
+++ src/main/java/com/supplyboost/Exercises/MultidimensionalArray/_3_DiagonalDifference.java
@@ -1,6 +1,5 @@
 package com.supplyboost.Exercises.MultidimensionalArray;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class _3_DiagonalDifference {
--- src/main/java/com/supplyboost/Exercises/MultidimensionalArray/_4_MaximalSum.java
+++ src/main/java/com/supplyboost/Exercises/MultidimensionalArray/_4_MaximalSum.java
@@ -1,6 +1,5 @@
 package com.supplyboost.Exercises.MultidimensionalArray;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class _4_MaximalSum {
