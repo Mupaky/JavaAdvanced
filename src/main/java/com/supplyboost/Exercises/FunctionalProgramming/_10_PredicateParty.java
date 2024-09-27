c8634dcc870bac46b6c30a169567ca3fe5307cb2
Mupaky <georgi.jelev94@gmail.com>
2024-09-27 11:50:59 +0300

    Adding Functional Programming Tasks
    - Task _10_ finished ....

--- src/main/java/com/supplyboost/Exercises/FunctionalProgramming/_10_PredicateParty.java
+++ src/main/java/com/supplyboost/Exercises/FunctionalProgramming/_10_PredicateParty.java
@@ -1,36 +1,54 @@
 package com.supplyboost.Exercises.FunctionalProgramming;
 
 import java.util.*;
-import java.util.function.Predicate;
+import java.util.function.BiPredicate;
 
 public class _10_PredicateParty {
 
-    //TODO the main method is not finished yet.... even thou is not separated by method, any way....
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        List<String> peopleComing = new ArrayList<>(Arrays
-//                                    .stream(scanner.nextLine().split("\\s"))
-//                                    .toList());
-//
-//        Map<String, Predicate<String>> operations = new HashMap<>();
-//        operations.put("StartWith", (word -> );
-//
-//
-//        String command = scanner.nextLine();
-//        while (!command.equals("Party!")){
-//            List<String> commands = new ArrayList<>(Arrays.stream(scanner.nextLine().split("\\s")).toList());
-//            switch (commands.getFirst()){
-//                case "Remove":
-//                    peopleComing.removeIf()
-//                    break;
-//                case "Double":
-//                    break;
-//                default:
-//                    break;
-//            }
-//
-//            command = scanner.nextLine();
-//        }
-//    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> peopleComing = new ArrayList<>(Arrays
+                                    .stream(scanner.nextLine().split("\\s"))
+                                    .toList());
+
+        Map<String, BiPredicate<String, String>> operations = new HashMap<>();
+        operations.put("StartsWith", String::startsWith);
+        operations.put("EndsWith", String::endsWith);
+        operations.put("Length", (word, length) -> word.length() == Integer.parseInt(length));
+
+
+        String command = scanner.nextLine();
+        while (!command.equals("Party!")){
+            List<String> commands = new ArrayList<>(Arrays.stream(command.split("\\s")).toList());
+            if(commands.get(1).equals("Length") && Integer.parseInt(commands.get(2)) < 0){
+                continue;
+            }
+            switch (commands.getFirst()){
+                case "Remove":
+                    peopleComing.removeIf(word -> operations.get(commands.get(1)).test(word, commands.get(2)));
+                    break;
+                case "Double":
+                    List<String> doubled = new ArrayList<>(peopleComing.stream()
+                            .takeWhile(word -> operations.get(commands.get(1)).test(word, commands.get(2)))
+                            .toList());
+                    peopleComing.addAll(doubled);
+                    break;
+                default:
+                    break;
+            }
+
+            command = scanner.nextLine();
+        }
+
+        //For the sake of the submission in SoftUni
+        Collections.sort(peopleComing);
+
+        if(peopleComing.isEmpty()){
+            System.out.println("Nobody is going to the party!");
+        }else{
+            String peopleList = peopleComing.toString().substring(1, peopleComing.toString().length()-1);
+            System.out.printf("%s are going to the party!", peopleList);
+        }
+    }
 }
